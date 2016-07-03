9d6d0ce525d76d349fa6eda93dae74e33d055656
Will <doggy.huang@gmail.com>
2016-07-04 03:21:07 +0800

    https://github.com/RickStrahl/AspNetCoreFromScratchSample

---
+++ src/AspNetFromScratchRTM/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            Configuration = builder.Build();
+        }
+   
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        private readonly ILogger<string> _logger;
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+        ILogger<string> logger)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            loggerFactory.AddConsole();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Pre Processing");
+
+            //    await next();
+
+            //    await context.Response.WriteAsync("Post Processing");
+            //});
+
+            // Handler of last Resort
+            app.Run(async (context) =>
+            {
+                context.Response.ContentType = "text/plain; charset=UTF-8";
+
+                logger.LogInformation(1, "Hello World of the last resort. The Time is: " +
+                    DateTime.Now.ToString("hh:mm:ss tt"));
+                    
+                await context.Response.WriteAsync(
+                    "Hello World of the last resort. The Time is: " +
+                    DateTime.Now.ToString("hh:mm:ss tt"));
+
+            });
+        }
+    }
+
+    public class HelloWorldController : Controller
+    {
+        [HttpGet("api/helloworld")]
+        public object HelloWorld()
+        {
+            return new
+            {
+                message = "Hello World",
+                time = DateTime.Now
+            };
+        }
+
+        [HttpGet("helloworld")]
+        public ActionResult HelloworldMvc()
+        {
+            ViewBag.Message = "Hello world!";
+            ViewBag.Time = DateTime.Now;
+
+            return View("helloworld");
+            //return View("~/helloworld.cshtml");
+        }
+    }
+}
