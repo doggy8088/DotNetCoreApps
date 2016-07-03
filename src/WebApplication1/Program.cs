ee849698c40196bf8a1c463e8184cd2763c0386a
Will <doggy.huang@gmail.com>
2016-07-04 01:47:42 +0800

    web project

---
+++ src/WebApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var contentRoot = Directory.GetCurrentDirectory();
+            var webRoot = Path.Combine(contentRoot, "wwwroot");
+
+            // http://stackoverflow.com/questions/36635003/how-do-we-set-contentrootpath-and-webrootpath
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(contentRoot)
+                .UseWebRoot(webRoot)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ src/WebApplication1/Properties/PublishProfiles/aaa-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = '0ebca9ed-1010-43c7-ad4a-d76c64e55037'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
